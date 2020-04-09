e6e9193dcfc20ac98c13deadea96b249de9b5ede
Daria Platonova <searching.for.a.sign@yandex.ru>
2020-04-09 17:51:37 +0300

    fix

--- src/about/index.js
+++ src/about/index.js
@@ -2,3 +2,5 @@ import "../pages/about.css";
 import inga from "../images/inga.png"
 import anton from "../images/anton.png"
 import mariia from "../images/mariia.png"
+import {swiper} from "../js/swiper/swiper.min.js"
+swiper();
