4fba79c6275d826d2e18ba9e13d91978655500fb
Daria Platonova <searching.for.a.sign@yandex.ru>
2020-04-09 23:30:32 +0300

    no css directory

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: './css/[name].[contenthash].css'
+      filename: '[name].[contenthash].css'
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
