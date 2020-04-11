b897446b9a0a14ecedd7444ad5f832676c560a8b
Daria Platonova <searching.for.a.sign@yandex.ru>
2020-04-12 00:55:34 +0300

    fixed css in one dir

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,14 @@ const webpack = require("webpack");
 const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
-  entry: { main: "./src/index.js",
-          analytics: "./src/analytics/index.js",
-          about: "./src/about/index.js" 
-
-},
+  entry: {
+    main: "./src/index.js",
+    analytics: "./src/analytics/index.js",
+    about: "./src/about/index.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[chunkhash].js"
+    filename: "[name].[chunkhash].js",
   },
 
   module: {
@@ -23,16 +23,16 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
         use: [
           isDev ? "style-loader" : MiniCssExtractPlugin.loader,
           "css-loader",
-          "postcss-loader"
-        ]
+          "postcss-loader",
+        ],
       },
 
       {
@@ -41,47 +41,47 @@ module.exports = {
           "file-loader?name=./images/[name].[ext]",
           {
             loader: "image-webpack-loader",
-            options: {}
-          }
-        ]
+            options: {},
+          },
+        ],
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: "file-loader?name=./vendor/[name].[ext]"
-      }
-    ]
+        loader: 'file-loader?name=../vendor/[name].[ext]'
+    }
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: './css/[name].[contenthash].css'
+      filename: "./css/[name].[contenthash].css",
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
       cssProcessor: require("cssnano"),
       cssProcessorPluginOptions: {
-        preset: ["default"]
+        preset: ["default"],
       },
-      canPrint: true
+      canPrint: true,
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      template: './src/index.html',
-      filename: 'index.html'
-  }),
-  new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
       inject: true,
-      template: './src/about.html',
-      filename: 'about.html'
-  }),
-  new HtmlWebpackPlugin({
-    inject: true,
-    template: './src/paper.html',
-    filename: 'paper.html'
-}),
+      template: "./src/about.html",
+      filename: "about.html",
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./src/paper.html",
+      filename: "paper.html",
+    }),
     new WebpackMd5Hash(),
 
     new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-    })
-  ]
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
 };
